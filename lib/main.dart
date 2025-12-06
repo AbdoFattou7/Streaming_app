2783137a05605397e411be71f6c54451f1293e59
OSSTeam <OSSTeam@gmail.com>
2025-12-06 15:58:49 +0200

    changed background color / added logo,drawer,actions

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,33 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
         appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.white),
+          actions: [
+            IconButton(
+              onPressed: null,
+
+              icon: Icon(Icons.account_circle, color: Colors.white),
+            ),
+          ],
           centerTitle: true,
-          title: Text("Egybest", style: TextStyle(fontWeight: FontWeight.bold)),
-          elevation: 1,
-          backgroundColor: const Color.fromARGB(255, 249, 242, 140),
+          title: SizedBox(
+            height: 30,
+            child: Image.asset('assets/images/Netflix logo.png'),
+          ),
+          elevation: 0,
+          backgroundColor: const Color.fromARGB(255, 0, 0, 0),
         ),
+        drawer: Drawer(child: ListView()),
         body: Center(
-          child: Text("This is the first page", style: TextStyle(fontSize: 30)),
+          child: Text(
+            "This is the first page",
+            style: TextStyle(
+              fontSize: 30,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
         ),
       ),
     );
